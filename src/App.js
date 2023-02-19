9be77ccb6659e9422f434118da7d64ebe38e2f04
Jason Zucchetto <jason@jasons-mbp.lan>
2023-02-18 22:05:56 -0800

    Updated styles, use MUI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,62 @@ import React, { useContext, useEffect, useState } from 'react';
 import './App.css';
 import Web3 from 'web3';
 
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const [walletData, setWalletData] = useState({
     walletAddress: "",
@@ -55,14 +109,41 @@ function App() {
   }
 
   return (
-    <div>
-      <button variant="success" size="lg" className="connectWallet green-btn" onClick={connect}>
-        {walletData.isConnected ?
-          walletData.walletAddress.substr(0, 4) + "..." + walletData.walletAddress.substr(-4)
-          : "Connect Wallet"}
-      </button><br/>
-      
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div style={{marginTop: "40px"}}>
+        <Container maxWidth="lg">
+
+          <div style={{textAlign: "right"}}>
+            <Button variant="outlined" size="lg" onClick={connect}>
+              {walletData.isConnected ?
+                walletData.walletAddress
+                : "Connect Wallet"}
+            </Button>
+          </div>
+
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                <Tab label="Overview" {...a11yProps(0)} />
+                <Tab label="Read Contract" {...a11yProps(1)} />
+                <Tab label="Write Contract" {...a11yProps(2)} />
+              </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+              CONTRACT_OVERVIEW
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              CONTRACT_READ_FUNCTIONS
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              CONTRACT_WRITE FUNCTIONS
+            </TabPanel>
+          </Box>
+        </Container>
+        
+      </div>
+    </ThemeProvider>
   );
 }
 
